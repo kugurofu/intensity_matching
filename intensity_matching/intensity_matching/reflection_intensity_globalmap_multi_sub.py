29bf1faca7c4d1bbe1825e24f5e117e1c9e89a20
kugurofu <tenten31569@icloud.com>
2026-07-28 10:49:23 +0000

    fix global map tf

--- intensity_matching/intensity_matching/reflection_intensity_globalmap_multi_sub.py
+++ intensity_matching/intensity_matching/reflection_intensity_globalmap_multi_sub.py
@@ -1,6 +1,7 @@
 # ROS2
 import rclpy
 from rclpy.node import Node
+from rclpy.duration import Duration
 # ROS msgs
 import std_msgs.msg as std_msgs
 import sensor_msgs.msg as sensor_msgs
@@ -242,7 +243,7 @@ class ObsBayesMap(Node):
         t0 = time.perf_counter()
 
         try:
-            transform = self.tf_buffer.lookup_transform("odom", "livox_frame", rclpy.time.Time.from_msg(ground_msg.header.stamp))
+            transform = self.tf_buffer.lookup_transform("odom", "livox_frame", rclpy.time.Time.from_msg(ground_msg.header.stamp), timeout=Duration(seconds=0.05))
 
         except TransformException as ex:
             self.get_logger().warn(f"TF lookup failed: {ex}")
